452fdfc24f20c72a8afa18ce5dd71c1cfa7deefe
Praduman Prajapati <tech.praduman@gmail.com>
2023-02-13 00:34:00 +0530

    day1.js

---
+++ day1.js
@@ -0,0 +1,82 @@
+// printing 
+console.log("Hello World")
+
+
+// variables-can't be a reserved keywords & be meaningful
+
+let a=5
+console.log(a)
+let b="Hello"
+console.log(b)
+// let can be updated but can't be re-declared
+
+var h="toy"
+console.log(h)
+// var can be updated and re-declared
+
+const e=654
+console.log(e)
+// const can't be re-declared and updated
+
+
+// primitive data types 
+// 1.string
+// 2.boolean
+// 3.undefined
+// 4.null
+
+
+// reference types 
+// 1.objects
+// 2.arrays-used to contain list of item
+// 3.functions
+
+
+// operators
+// 1.arithmetic
+// 2.assignment
+// 3.comparison
+// 4.bitwise
+// 5.logical
+
+
+// ternary operator
+let age =17
+let status= (age>=10) ? 'yes drive': 'no vote'
+console.log(status)
+
+
+// logical operator
+// AND, OR & NOT
+
+
+// control statement 
+// 1.if-else 
+// 2.switch 
+
+
+// loops
+// 1.for loop
+// 2.while loop
+// 3.do-while loop
+// 4.what is an infinite loop
+// 5.for-in loop
+// 6.for-of loop
+
+
+for(let i=0;i<5;i++){
+    console.log(i)
+}
+
+let o=1
+while(o<7){
+    console.log(o)
+    o++
+}
+
+let t=2
+do{
+    console.log(t)
+    t++
+}
+while(t<9)

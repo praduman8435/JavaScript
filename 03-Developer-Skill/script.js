6122155a5aa66b85b6e0ae369b0496e578fe3f4a
Praduman Prajapati <tech.praduman@gmail.com>
2023-04-17 20:51:37 +0530

    Update script.js

--- 03-Developer-Skill/script.js
+++ 03-Developer-Skill/script.js
@@ -1,14 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-/*
 ///////////////////////////////////////
+
 // Using Google, StackOverflow and MDN
 
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem
 // - What is temp amplitude? Answer: difference between highest and lowest temp
@@ -21,22 +21,22 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
 
-const calcTempAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
 
 // PROBLEM 2:
 // Function should now receive 2 arrays of temps
@@ -66,7 +66,6 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
-
 
 ///////////////////////////////////////
 // Debugging with the Console and Breakpoints
@@ -114,11 +113,11 @@ const calcTempAmplitudeBug = function (t1, t2) {
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
 // A) IDENTIFY
 console.log(amplitudeBug);
-*/
+
 
 ///////////////////////////////////////
-// Coding Challenge #1
 
+// Coding Challenge #1
 /*
 Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
 
@@ -133,16 +132,17 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 */
 
 /*
-// 1) Understanding the problem
-// - Array transformed to string, separated by ...
-// - What is the X days? Answer: index + 1
+1) Understanding the problem
+- Array transformed to string, separated by ...
+- What is the X days? Answer: index + 1
 
-// 2) Breaking up into sub-problems
-// - Transform array into string
-// - Transform each element to string with ºC
-// - Strings needs to contain day (index + 1)
-// - Add ... between elements and start and end of string
-// - Log string to console
+2) Breaking up into sub-problems
+- Transform array into string
+- Transform each element to string with ºC
+- Strings needs to contain day (index + 1)
+- Add ... between elements and start and end of string
+- Log string to console
+*/
 
 const data1 = [17, 21, 23];
 const data2 = [12, 5, -5, 0, 4];
@@ -157,5 +157,3 @@ const printForecast = function (arr) {
   console.log('...' + str);
 };
 printForecast(data1);
-*/
-
